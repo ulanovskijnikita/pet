d2171850ec9f76fa39c5777eac578429b79d428c
ulanovskijnikita <ulanovskijnikita@gmail.com>
2025-06-04 16:24:27 +0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pet/',
+  base: './pet/',
   esbuild: {
     target: 'es2022',
     supported: {
