9ea0dfe9756bda3dcc4e788b214e70bda7c50f95
ulanovskijnikita <ulanovskijnikita@gmail.com>
2025-06-04 14:04:24 +0500

    всё забилдилось

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: 'es2022',
+    supported: {
+      'top-level-await': true
+    },
+  },
+  build: {
+    target: 'es2022'
+  },
   plugins: [
     react(),
     tailwindcss(),
