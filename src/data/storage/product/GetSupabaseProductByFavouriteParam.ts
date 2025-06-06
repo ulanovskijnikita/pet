6faa7f2cea3c592eb83c8e2006fda46db8a4eb63
ulanovskijnikita <ulanovskijnikita@gmail.com>
2025-06-06 14:42:15 +0500

    перещён файл в папку

--- src/data/storage/product/GetSupabaseProductByFavouriteParam.ts
+++
@@ -1,7 +0,0 @@
-import SupabaseProductRange from "../model/product/SupabaseProductRange";
-import { SupabaseUserId } from "../model/user/SupabaseUser";
-
-export default interface GetSupabaseProductByFavouriteParam extends SupabaseProductRange {
-
-    id: SupabaseUserId
-}
