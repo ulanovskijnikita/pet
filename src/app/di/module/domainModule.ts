4b03d3e8812793c313ddf49247f1b75a3378952e
ulanovskijnikita <ulanovskijnikita@gmail.com>
2025-06-07 11:58:07 +0500

    удален неиспользуемый usecase

--- src/app/di/module/domainModule.ts
+++ src/app/di/module/domainModule.ts
@@ -1,7 +1,6 @@
 import {ContainerModule} from "inversify";
 import GetProductByCategoryUseCase from "../../../domain/usecase/GetProductByCategoryUseCase.ts";
 import ProductRepositoryImpl from "../../../data/repository/ProductRepositoryImpl.ts";
-import SearchProductByTagUseCase from "../../../domain/usecase/SearchProductByTagUseCase.ts";
 import GetSecondUseCase from "../../../domain/usecase/GetSecondUseCase.ts";
 import EmailServiceImpl from "../../../infrastructure/email/EmailServiceImpl.ts";
 import SignInUserUseCase from "../../../domain/usecase/SignInUserUseCase.ts";
@@ -32,15 +31,6 @@ const domainModule = new ContainerModule(
 
                 context =>
                     new GetProductByCategoryUseCase( context.get(ProductRepositoryImpl) )
-            )
-            .inTransientScope()
-
-        options
-            .bind<SearchProductByTagUseCase>(SearchProductByTagUseCase)
-            .toDynamicValue(
-
-                context =>
-                    new SearchProductByTagUseCase( context.get(ProductRepositoryImpl) )
             )
             .inTransientScope()
 
